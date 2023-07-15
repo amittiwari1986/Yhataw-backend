6889632f37863c15a5a0479425b8b6874b0a0866
amit <infoindian.amit@gmail.com>
2023-07-15 10:46:54 -0700

    add user document

---
+++ dto/userdocumentto.js
@@ -0,0 +1,13 @@
+class UserDocument{
+    constructor(userId,aadhar,pan,passport,medical,voterId,others){
+        this.stateId = stateId
+        this.aadhar = aadhar
+        this.pan = pan
+        this.passport = passport
+        this.medical = medical
+        this.voterId = voterId
+        this.others = others
+    }
+}
+
+module.exports = UserDocument;
