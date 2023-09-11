cde9d83ae665ee28659b08afc6defafb1fedd0b8
amit <infoindian.amit@gmail.com>
2023-09-11 00:57:14 -0700

    add correction on document

--- db/models/UserDocumentModel.js
+++ db/models/UserDocumentModel.js
@@ -3,12 +3,12 @@ const connection  = require('../connect');
  const {Schema} = require('mongoose');
  const UserDocumentSchema = new Schema({
         userId:{type:Schema.Types.String,required:true,ref: "users"},
-     aadhar:{type:Schema.Types.String,required:true},
-     pan:{type:Schema.Types.String,required:true},
-     passport:{type:Schema.Types.String,required:true},
-     medical:{type:Schema.Types.String,required:true},
-     voterId:{type:Schema.Types.String,required:true},
-     others:{type:Schema.Types.String,required:true},
+     aadhar:{type:Schema.Types.String,required:false},
+     pan:{type:Schema.Types.String,required:false},
+     passport:{type:Schema.Types.String,required:false},
+     medical:{type:Schema.Types.String,required:false},
+     voterId:{type:Schema.Types.String,required:false},
+     others:{type:Schema.Types.String,required:false},
      createdAt:{type:Schema.Types.Date,default:new Date().toString()}
  },{timestamps:true}); 
  
