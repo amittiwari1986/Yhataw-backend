cdd6e3bce65e09395a9964828976f07e1f3083d1
amit <infoindian.amit@gmail.com>
2023-09-19 04:13:17 -0700

    add new filter in leave and attendence

--- services/userLeaveService.js
+++ services/userLeaveService.js
@@ -23,7 +23,33 @@ const userLeaveSerives = {
     async getUserLeaveById(id){
         const promise = await UserLeaveModel.findById(id)
         return promise
-    }
+    },
+    async getAllLeave(query){
+         const promise = await UserLeaveModel.aggregate(
+                [
+                {
+                    "$match": {"userId":query.userId,"from_date": {"$gte": query.start_date, "$lte": query.end_date}}
+                },
+                { "$project": {
+                    "userId": { "$toObjectId": "$userId" },
+                    "leave_type": { "$toString": "$leave_type" },
+                    "from_date": { "$toString": "$from_date" },
+                    "to_date": { "$toString": "$to_date" },
+                    "comments": { "$toString": "$comments" },
+                    "total_days": { "$toString": "$total_days" },
+                    "status": { "$toString": "$status" },
+                    "createdAt": { "$toString": "$createdAt" },
+                    "updatedAt": { "$toString": "$updatedAt" },
+                }},
+                    {$lookup: 
+                        {from: "users", 
+                        localField: "userId", 
+                        foreignField: "_id", 
+                        as: "users"}
+                    },
+                    { $sort : { updatedAt : -1} }])
+            return promise
+    },
 }
 
 module.exports = userLeaveSerives;
