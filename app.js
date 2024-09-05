097e7b56f4b6ff0a381bb0c53b0599b3301858ab
amit <infoindian.amit@gmail.com>
2024-09-05 23:13:44 +0530

    ss

--- app.js
+++ app.js
@@ -3,24 +3,6 @@ const app = express()
 const cors = require("cors")
 const dotenv = require("dotenv")
 const  morgan = require('morgan')
-
-// Requiring in-built https for creating
-// https server
-const https = require("https");
-
-
-// Requiring file system to use local files
-const fs = require("fs");
-
-// Parsing the form of body to take
-// input from forms
-const bodyParser = require("body-parser");
-
-// Configuring express to use body-parser
-// as middle-ware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 dotenv.config()
 //middleWares
 app.use(cors())
@@ -33,17 +15,6 @@ app.use("/",require("./routes/masterroute"))
 app.use("/",require("./routes/projectroute"))
 app.use("/",require("./routes/leadroute"))
 app.use("/",require("./routes/reportroute"))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-// Creating object of key and certificate
-// for SSL
-const options = {
-    key: fs.readFileSync("server.key"),
-    cert: fs.readFileSync("server.cert"),
-};
-const server = https.createServer(options, app).listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log("Backend Up",server.address().port)
 })
